f257e7d8c58109c43afa05f6e35312bf5f8151a1
vivekmit05 <vivek.mit05@gmail.com>
2018-03-19 01:06:45 +0530

    Increment and Decrement Custom Date Picker Using Motion Events and Touch Listener

--- app/src/main/java/com/bucketdrops/vivek/bucketdrops/widgets/BucketPickerView.java
+++ app/src/main/java/com/bucketdrops/vivek/bucketdrops/widgets/BucketPickerView.java
@@ -1,9 +1,12 @@
 package com.bucketdrops.vivek.bucketdrops.widgets;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,12 +20,17 @@ import java.util.Calendar;
  * Created by vivek on 3/16/2018.
  */
 
-public class BucketPickerView extends LinearLayout {
+public class BucketPickerView extends LinearLayout implements View.OnTouchListener {
     private Calendar mCalendar;
     private TextView mTextDate;
     private TextView mTextMonth;
     private TextView mTextYear;
     private SimpleDateFormat mformatter;
+
+    public static final int LEFT=0;
+    public static final int TOP=1;
+    public static final int RIGHT=2;
+    public static final int BOTTOM=3;
 
     public BucketPickerView(Context context) {
         super(context);
@@ -51,6 +59,10 @@ public class BucketPickerView extends LinearLayout {
         mTextMonth=(TextView) findViewById(R.id.tv_month);
         mTextYear=(TextView) findViewById(R.id.tv_year);
 
+        mTextDate.setOnTouchListener(this);
+        mTextMonth.setOnTouchListener(this);
+        mTextYear.setOnTouchListener(this);
+
         int date=mCalendar.get(Calendar.DATE);
         int month=mCalendar.get(Calendar.MONTH);
         int year=mCalendar.get(Calendar.YEAR);
@@ -74,4 +86,117 @@ public class BucketPickerView extends LinearLayout {
     public long getTime(){
         return mCalendar.getTimeInMillis();
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch(v.getId())
+        {
+            case R.id.tv_date:
+                processEventsFor(mTextDate,event);
+                break;
+            case R.id.tv_month:
+                processEventsFor(mTextMonth,event);
+                break;
+            case R.id.tv_year:
+                processEventsFor(mTextYear,event);
+                break;
+        }
+        return false;
+    }
+
+    private void processEventsFor(TextView textView, MotionEvent event) {
+        Drawable[] drawables=textView.getCompoundDrawables();
+        if(hasDrawableTop(drawables) && hasDrawableBottom(drawables)){
+            Rect topBounds = drawables[TOP].getBounds();
+            Rect bottomBounds=drawables[BOTTOM].getBounds();
+            float x=event.getX();
+            float y=event.getY();
+            if(topDrawableHit(textView,topBounds.height(),x,y)){
+                if(isActionDown(event)){
+                    increment(textView.getId());
+                }
+            }
+            else if(bottomDrawableHit(textView,bottomBounds.height(),x,y)){
+                if(isActionDown(event)){
+                    decrement(textView.getId());
+                }
+            }
+            else{
+
+            }
+        }
+    }
+
+
+    private void increment(int id) {
+        switch(id){
+            case R.id.tv_date:
+                mCalendar.add(Calendar.DATE,1);
+                break;
+            case R.id.tv_month:
+                mCalendar.add(Calendar.MONTH,1);
+                break;
+            case R.id.tv_year:
+                mCalendar.add(Calendar.YEAR,1);
+                break;
+        }
+        set(mCalendar);
+    }
+
+    private void set(Calendar calendar) {
+        int date=calendar.get(Calendar.DATE);
+        int year=calendar.get(Calendar.YEAR);
+
+        mTextDate.setText(date+"");
+        mTextMonth.setText(mformatter.format(mCalendar.getTime())); //To format the date in abrrevated month format
+        mTextYear.setText(year+"");
+    }
+
+    private void decrement(int id) {
+        switch (id) {
+            case R.id.tv_date:
+                mCalendar.add(Calendar.DATE, -1);
+                break;
+            case R.id.tv_month:
+                mCalendar.add(Calendar.MONTH, -1);
+                break;
+            case R.id.tv_year:
+                mCalendar.add(Calendar.YEAR, -1);
+                break;
+        }
+        set(mCalendar);
+    }
+
+    private boolean isActionDown(MotionEvent event) {
+        return event.getAction()==MotionEvent.ACTION_DOWN;
+    }
+
+    private boolean topDrawableHit(TextView textView,int drawableHeight,float x, float y) {
+        int xmin=textView.getPaddingLeft();
+        int xmax=textView.getWidth()-textView.getPaddingRight();
+        int ymin=textView.getPaddingTop();
+        int ymax=textView.getPaddingTop()+drawableHeight;
+
+        return  x>xmin && x<xmax && y>ymin && y<ymax;
+    }
+
+    private boolean bottomDrawableHit(TextView textView,int drawableHeight,float x, float y) {
+        int xmin=textView.getPaddingLeft();
+        int xmax=textView.getWidth()-textView.getPaddingRight();
+        int ymax=textView.getHeight()-textView.getPaddingBottom();
+        int ymin=ymax-drawableHeight;
+
+
+        return  x>xmin && x<xmax && y>ymin && y<ymax;
+    }
+
+    private boolean hasDrawableTop(Drawable[] drawables){
+        return drawables[TOP]!=null;
+    }
+
+    private boolean hasDrawableBottom(Drawable[] drawables){
+        return drawables[BOTTOM]!=null;
+    }
+
+
 }
